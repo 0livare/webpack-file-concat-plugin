6b64a8a729f3ab3eb104321485bf24dc02c736d5
Zach Posten <zach@posten.io>
2020-09-02 10:55:13 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const fs = require('fs-extra')
+const glob = require('glob')
+
+const validateOptions = require('./validateOptions')
+
+const defaultOptions = {deleteInputFiles: false, appendToDestination: true}
+
+module.exports = class FileConcatenationPlugin {
+  constructor(options) {
+    validateOptions(options)
+    this.options = {...defaultOptions, ...options}
+  }
+
+  apply(compiler) {
+    let webpackOutputPath = compiler.options.output.path
+    this.destinationFilePath = path.join(
+      webpackOutputPath,
+      this.options.destination,
+    )
+    this.sourceFilesGlob = path.join(webpackOutputPath, this.options.source)
+
+    compiler.hooks.afterEmit.tapPromise(
+      'FileConcatenationPlugin',
+      this.run.bind(this),
+    )
+  }
+
+  async run(compilation) {
+    let inputFiles = await this.performGlobSearch(this.sourceFilesGlob)
+
+    let allInputFileContents = ''
+    for (let inputFilePath of inputFiles) {
+      let inputFileContents = await fs.readFile(inputFilePath)
+      allInputFileContents += inputFileContents
+
+      if (this.options.deleteInputFiles) {
+        await fs.remove(inputFilePath)
+      }
+    }
+
+    if (this.options.appendToDestination) {
+      await fs.appendFile(this.destinationFilePath, allInputFileContents)
+    } else {
+      await fs.writeFile(this.destinationFilePath, allInputFileContents)
+    }
+  }
+
+  performGlobSearch(globStr) {
+    return new Promise(resolve => {
+      glob(globStr, null, function (er, files) {
+        resolve(files)
+      })
+    })
+  }
+}
---
+++ validateOptions.js
@@ -0,0 +1,49 @@
+const pluginName = 'FileConcatenationPlugin'
+const invalidOptionErrorText = `Invalid options passed to ${pluginName}`
+
+const requiredOptions = {
+  destination: 'string',
+  source: 'string',
+}
+
+const optionalOptions = {
+  deleteInputFiles: 'boolean',
+  appendToDestination: 'boolean',
+}
+
+module.exports = function validateOptions(options) {
+  if (!options) {
+    throw new Error(
+      `Options object must be passed to ${pluginName} constructor`,
+    )
+  }
+
+  let requiredOptionNames = Object.keys(requiredOptions)
+  for (let requiredOptionName of requiredOptionNames) {
+    let passedOption = options[requiredOptionName]
+    if (!passedOption) {
+      throw new Error(
+        `${invalidOptionErrorText}: "${requiredOptionName}" option is required`,
+      )
+    }
+  }
+
+  let allOptions = {...requiredOptions, ...optionalOptions}
+  for (let passedOptionName of Object.keys(options)) {
+    let optionExists = !!allOptions[passedOptionName]
+    if (!optionExists) {
+      throw new Error(
+        `${invalidOptionErrorText}: Unknown option "${passedOptionName}"`,
+      )
+    }
+
+    let passedOptionType = typeof options[passedOptionName]
+    let expectedTypeForOption = allOptions[passedOptionName]
+
+    if (passedOptionType !== expectedTypeForOption) {
+      throw new Error(
+        `${invalidOptionErrorText}: ${passedOptionName} must of of type ${expectedTypeForOption}, but found ${passedOptionType}`,
+      )
+    }
+  }
+}
